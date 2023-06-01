4a9140402d9bfee5b36f86c6915f05c4545bc382
Ivan Lesko <ivanlesko@gmail.com>
2023-06-01 16:00:28 -0700

    Adds 103. Binary Tree Zigzag Level Order Traversal

--- 103. Binary Tree Zigzag Level Order Traversal .playground/Contents.swift
+++ 103. Binary Tree Zigzag Level Order Traversal .playground/Contents.swift
@@ -15,12 +15,12 @@ public class TreeNode {
 
 /**
  # Intuition
- Keep a running total of the current row using BFS
+ Use the normal BFS traversal and reverse each odd row
 
  # Approach
- Use BFS to iterate over each row in the tree.
- Keep a running total of each row.
- Reset the total to 0 at the start of each row.
+ Iterate using standard BFS traversal.
+ Create an array of values for each row.
+ Reverse the row if it is an odd row.
 
  # Complexity
  - Time complexity:
@@ -29,3 +29,34 @@ public class TreeNode {
  - Space complexity:
  O(n/2 + 1) or O(n)
  */
+
+class Solution {
+    func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else {
+            return []
+        }
+        
+        var queue = [root], res = [[Int]]()
+        var isEven = true
+        while !queue.isEmpty {
+            var acc = [Int]()
+            for _ in 0..<queue.count {
+                let node = queue.removeFirst()
+                acc.append(node.val)
+                
+                if let left = node.left {
+                    queue.append(left)
+                }
+                
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+            
+            res.append(isEven ? acc : acc.reversed())
+            isEven = !isEven
+        }
+        
+        return res
+    }
+}
