f602df98cc4be8ba78a8cdf94cd333eb34263645
Ivan Lesko <ivanlesko@gmail.com>
2023-06-02 08:07:45 -0700

    Adds 107. Binary Tree Level Order Traversal II

---
+++ 107. Binary Tree Level Order Traversal II .playground/Contents.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+/**
+ # Intuition
+ Store an array of each row's values in reverse order.
+
+ # Approach
+ Use BFS to iterate over each row in the tree.
+ Append each row array to the end of result.
+
+ # Complexity
+ - Time complexity:
+ O(n) where `n` is `root.nodes.size`
+
+ - Space complexity:
+ O(n/2 + 1) or O(n)
+ */
+
+class Solution {
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        guard let root = root else {
+            return []
+        }
+        
+        var res = [[Int]](), queue = [root]
+        while !queue.isEmpty {
+            var acc = [Int]()
+            for _ in 0..<queue.count {
+                let node = queue.removeFirst()
+                acc.append(node.val)
+                
+                if let left = node.left {
+                    queue.append(left)
+                }
+                
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+            res.insert(acc, at: 0)
+            acc = []
+        }
+        
+        return res
+    }
+}
