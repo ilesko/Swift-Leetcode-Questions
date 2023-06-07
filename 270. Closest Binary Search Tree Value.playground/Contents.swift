09895f23237519ec08289d3412597dcc1ce03dd8
Ivan Lesko <ivanlesko+github@gmail.com>
2023-06-07 07:26:17 -0700

    Updates 270. Closest Binary Search Tree Value with a better solution and description

--- 270. Closest Binary Search Tree Value.playground/Contents.swift
+++ 270. Closest Binary Search Tree Value.playground/Contents.swift
@@ -15,39 +15,71 @@ public class TreeNode {
 
 /**
  # Intuition
- Brute force check each node
+  Traverse along a path in a binary search tree where `root.val` is closest to `target`.
 
- # Approach
- Use inorder DFS to traverse to each node.
- Find the absolute difference between `root.val` and `target`.
- Store the minimum difference found.
+  # Approach
+  Use BST traversal to path along the values that are closest to `target`.
+  Store the minimum delta between `root.val` and `target`.
+  Store the smallest visited node value.
+  
+  Update the smallest visited node when the current delta is less than the minimum delta.
+  Update the smallest visited node when the current delta is equal to the current delta.  Use the `min()` of these two nodes.
 
- # Complexity
- - Time complexity:
- O(n) where `n` is `root.nodes.size`
+  # Complexity
+  - Time complexity:
+  O(log n) where `n` is `root.nodes.size`
+  O(n) worst case when the tree is unbalanced (like a one sided linked list)
 
- - Space complexity:
- O(1) or O(n) where `n` is the depth of the call stack
+  - Space complexity:
+  O(1) or O(h) where `h` is the height of the call stack
+
+ Take this tree for example:
+ ```
+ Target: 7.5
+
+          9
+        /   \
+     4       10
+    / \      / \
+   3    7   11  12
+  /    / \
+ 1    6   8
+
+ ```
+
+ The traversal path will be `9 -> 4 -> 7 -> 8`
+
+ 9's right subtree can be ignored since 9 > 7.5 and any of the values in the right subtree will always be greater than 9.
+
+ 7 and 8 will both be valid answer after following the BST traversal path. We can store the lowest node value since both of the deltas are equal (0.5).
  */
 
 class Solution {
-    func evaluate(_ root: TreeNode?, _ target: Double, _ min: inout Double, _ res: inout Int) {
+    var minDelta = Double(Int.max)
+    var ans = Int.max
+    
+    func evaluate(_ root: TreeNode?, _ target: Double) {
         guard let root = root else {
             return
         }
         
-        evaluate(root.left, target, &min, &res)
-        let val = abs(target - Double(root.val))
-        if val < min {
-            min = val
-            res = root.val
+        let delta = abs(target - Double(root.val))
+        if delta < minDelta {
+            minDelta = delta
+            ans = root.val
+        } else if delta == minDelta {
+            ans = min(ans, root.val)
         }
-        evaluate(root.right, target, &min, &res)
+        
+        if target < Double(root.val) {
+            evaluate(root.left, target)
+        } else {
+            evaluate(root.right, target)
+        }
     }
     
     func closestValue(_ root: TreeNode?, _ target: Double) -> Int {
-        var min = Double(Int.max), res = 0
-        evaluate(root, target, &min, &res)
-        return res
+        evaluate(root, target)
+        return ans
     }
 }
